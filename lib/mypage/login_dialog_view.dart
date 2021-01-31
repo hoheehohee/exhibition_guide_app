33957b7421107538ae30e3207c935d01fea9fab1
곽영호 <1080609@hanwha.com>
2021-01-31 18:41:57 +0900

    동의 화면 추가

--- lib/mypage/login_dialog_view.dart
+++ lib/mypage/login_dialog_view.dart
@@ -12,7 +12,6 @@ class LoginDialogView extends StatefulWidget {
 }
 
 class _LoginDialogViewState extends State<LoginDialogView> {
-
   @override
   Widget build(BuildContext context) {
     final _social = Provider.of<SocialProvider>(context);
@@ -24,12 +23,11 @@ class _LoginDialogViewState extends State<LoginDialogView> {
     return Container(
       padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(30),
-          topLeft: Radius.circular(30),
-        )
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          )),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -52,15 +50,12 @@ class _LoginDialogViewState extends State<LoginDialogView> {
                 flex: 1,
                 child: Align(
                   alignment: Alignment(-0.2, 1),
-                  child: Text(
-                      '로그인',
+                  child: Text('로그인',
                       style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
                           color: Colors.black,
-                          decoration: TextDecoration.none
-                      )
-                  ),
+                          decoration: TextDecoration.none)),
                 ),
               )
             ],
@@ -103,8 +98,11 @@ class _LoginDialogViewState extends State<LoginDialogView> {
                 height: 12,
               ),
               MaterialButton(
-                  onPressed: () {
-                    _social.naverLogin();
+                  onPressed: () async {
+                    var login = await _social.naverLogin();
+                    if (login["check"] == "N") {
+                      Get.dialog(AgreeDialogView(login['snsType'], login['email']));
+                    }
                   },
                   color: kNaverColor,
                   minWidth: double.infinity,
@@ -117,8 +115,7 @@ class _LoginDialogViewState extends State<LoginDialogView> {
                       Image.asset("assets/images/naver_icon.png"),
                       SizedBox(width: 8),
                       Text('Sign up With Naver',
-                          style: TextStyle(
-                              fontSize: 16, color: kWhiteColor)),
+                          style: TextStyle(fontSize: 16, color: kWhiteColor)),
                     ],
                   )),
               SizedBox(
@@ -133,26 +130,25 @@ class _LoginDialogViewState extends State<LoginDialogView> {
                 padding: EdgeInsets.symmetric(vertical: 16),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8)),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/images/facebook_logo.png"),
-                      SizedBox(width: 8),
-                      Text(
-                        "Sign up with Facebook",
-                        style:
-                        TextStyle(fontSize: 16, color: kWhiteColor),
-                      )
-                    ]),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset("assets/images/facebook_logo.png"),
+                  SizedBox(width: 8),
+                  Text(
+                    "Sign up with Facebook",
+                    style: TextStyle(fontSize: 16, color: kWhiteColor),
+                  )
+                ]),
               ),
               SizedBox(
                 height: 12,
               ),
               MaterialButton(
-                  onPressed:() async {
+                  onPressed: () async {
                     var login = await _social.googleLogin();
-                    if(login["check"] == "N"){
-                      Get.dialog(AgreeDialogView(login['snsType'], login['email']));
+                    if (login["check"] == "N") {
+                      Get.dialog(
+                          AgreeDialogView(login['snsType'], login['email']));
                     }
                   },
                   color: kWhiteColor,
